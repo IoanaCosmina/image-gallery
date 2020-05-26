d5631cba50d6ce459693e9a0d86cb372382901d6
unknown <andriescuioana@gmail.com>
2020-05-26 09:49:49 +0200

    Updates

---
+++ precache-manifest.564d5fdd5dc532f3b74e4d4e540325e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5225f3c17b4f9e7324ab00aacf5b70",
+    "url": "/image-gallery/index.html"
+  },
+  {
+    "revision": "8d8ebaf0ce83b12f1010",
+    "url": "/image-gallery/static/css/main.7454b250.chunk.css"
+  },
+  {
+    "revision": "2e27511ec69e0c3c7eb4",
+    "url": "/image-gallery/static/js/2.3285e4d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-gallery/static/js/2.3285e4d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8ebaf0ce83b12f1010",
+    "url": "/image-gallery/static/js/main.e38a39da.chunk.js"
+  },
+  {
+    "revision": "bb958fcdea5eda83dc8a",
+    "url": "/image-gallery/static/js/runtime-main.b8f7f8c5.js"
+  }
+]);
